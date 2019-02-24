0ae461a7c646298affc3339739e27cea02b2418b
Brent Waters <bcwaters@asu.edu>
2019-02-23 21:35:30 -0700

    Removed rounded border and shadows from components

--- src/tempEmailClient/components/InboxSidebar.js
+++ src/tempEmailClient/components/InboxSidebar.js
@@ -19,16 +19,12 @@ const styles = theme => ({
         padding: '0px', 
         minWidth:'0px'},
     listItem:{
-        backgroundColor: 'lightgoldenrodyellow',
+        backgroundColor: '#ffffff',
         borderBottomWidth: '1px',
         borderBottomStyle: 'solid',
         borderRadius: '0px 0px 15px 15px',      
     },
     listBorder:{
-        backgroundColor: 'palegoldenrod',
-        borderRadius: '15px 15px 15px 15px',
-        borderWidth: '1px 1px 0px 1px',
-        borderStyle: 'solid',
         padding: '0px',
     },
     inboxIcon:{
@@ -105,9 +101,9 @@ class InboxSidebar extends React.Component {
                  
             <ListItem style={{ paddingBottom: '0px',
                                     zIndex: '1',
-                                    boxShadow: 'inset hsl(50, 48%, 55%) 1px 0px 9px 3px',
-                                    borderRadius: "15px 15px 0px 0px",
-                                    backgroundColor: 'palegoldenrod',
+                               
+                                   
+                                    backgroundColor: '#cccccc',
                                     borderWidth: '0px 0px 1px 0px', 
                                     minHeight: '40px',
                                     borderStyle: 'solid'}}>
